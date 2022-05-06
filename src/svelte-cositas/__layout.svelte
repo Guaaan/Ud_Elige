32226234cdc40a6956d1ff69199c90cfdf38878e
Juan Rodriguez <juan.rodriguez@redfarma.cl>
2022-05-06 16:31:40 -0400

    send

---
+++ src/svelte-cositas/__layout.svelte
@@ -0,0 +1,180 @@
+<script>
+  import "../app.scss";
+  import 'sweetalert2/src/sweetalert2.scss'
+  import { onMount } from "svelte";  
+  import { user } from "$lib/store.js";
+  import { routeToPage } from "$lib/routeToPage.js";
+  import PageHeader from "$lib/PageHeader.svelte";
+  import { page } from '$app/stores'; // para obtener el nombre de la vista
+  import BarraStickyBottom from "$lib/BarraStickyBottom.svelte";
+
+  // Redireccion
+  onMount(() => {
+    if ($user == null) routeToPage("login", true);
+  });
+
+
+</script>
+
+<svelte:head>
+  <link href="/css/all.min.css" rel="stylesheet" />
+  <link href="/css/fontawesome.css" rel="stylesheet" />
+  <link href="/css/default.css" rel="stylesheet" />
+
+  <link href="/css/nice-select.css" rel="stylesheet" />
+  <link href="/css/jquery.nice-number.css" rel="stylesheet" />
+
+  <link
+    href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css"
+    rel="stylesheet"
+    type="text/css"
+  />
+
+  <link href="/css/style.css" rel="stylesheet" />
+  <link href="/css/responsive.css" rel="stylesheet" />
+</svelte:head>
+
+<div class="overlay" />
+
+<PageHeader></PageHeader>
+
+<slot />
+
+{#if $page.routeId != "cart"}
+  <BarraStickyBottom></BarraStickyBottom>
+{/if}
+<!-- footer-area-start -->
+<div class="footer-area">
+  <div id="footer-escritorio" class="d-xxl-block d-xl-block d-lg-block d-md-none d-sm-none d-xs-none">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="footer-fl">
+            <div class="footer-logo">
+              <a href="index.html"><img src="/img/site-logo.png" alt="" /></a>
+            </div>
+            <div class="single-footer">
+              <h3>Menu</h3>
+              <ul>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >QUIENES SOMOS</a>
+                </li>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >MISIÓN Y VISIÓN</a>
+                </li>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >CONTACTO</a>
+                </li>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >TÉRMINO Y CONDICIONES</a>
+                </li>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >RECLAMOS</a>
+                </li>
+              </ul>
+            </div>
+            <div class="single-footer">
+              <h3>Informacion</h3>
+              <ul>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >REGLAMENTO DE FARMACIAS</a>
+                </li>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >REGLAMENTO PRODUCTOS FARMACÉUTICOS</a>
+                </li>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >SUGERENCIAS Y RECLAMOS</a>
+                </li>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >TÉRMINO Y CONDICIONES</a>
+                </li>
+                <li>
+                  <a href="/#" onClick={(e) => e.preventDefault()} >LIBRO DE RECLAMOS</a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer-socail">
+              <a href="/#" onClick={(e) => e.preventDefault()} ><i class="fab fa-facebook" /></a>
+              <a href="/#" onClick={(e) => e.preventDefault()} ><i class="fab fa-twitter" /></a>
+              <a href="/#" onClick={(e) => e.preventDefault()} ><i class="fab fa-instagram" /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="footer-mobile" class="d-xxl-none d-xl-none d-lg-none d-md-block d-sm-block d-xs-block">
+      <div class="container">
+        <div class="row">          
+          <div class="col-3">
+            <a href="/#" onClick={(e) => e.preventDefault()}>
+              <img src="/img/a1.png" alt="">
+              <p class="text-white">QUIENES SOMOS</p>
+            </a>
+          </div>
+          <div class="col-3">
+            <a href="/#" onClick={(e) => e.preventDefault()}>
+              <img src="/img/a2.png" alt="">
+              <p class="text-white">MISIÓN Y VISIÓN</p>
+            </a>
+          </div>
+          <div class="col-3">
+            <a href="/#" onClick={(e) => e.preventDefault()}>
+              <img src="/img/a3.png" alt="">
+              <p class="text-white">CONTACTO</p>
+            </a>
+          </div>
+          <div class="col-3">
+            <a href="/#" onClick={(e) => e.preventDefault()}>
+              <img src="/img/a4.png" alt="">
+              <p class="text-white">RECLAMOS</p>
+            </a>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
+
+<!-- footer-area-end -->
+<style>
+  .header-menu li a,
+  .header-user a {
+    text-decoration: none;
+  }
+  .footer-area a {
+    text-decoration: none;
+  }
+  .header-area {
+    height: 110px;
+    background: #ef0840;
+    display: flex;
+    align-items: center;
+    transition: 0.3s;
+  }
+  .header-area.active {
+    margin-top: -110px;
+  }
+  .footer-area{
+    padding: 44px 10px 44px 0;
+    /* margin-top: 100px; */
+  }
+  .footer-logo{
+    margin-top: -20px;
+  }
+  #footer-mobile .row{
+    text-align:center;
+  }
+
+@media only screen and (min-width: 320px) and (max-width: 767px) {
+  #footer-escritorio{
+    display: none;
+  }
+  #footer-mobile{
+    display: block;
+  }
+  .footer-area{
+    padding: 25px 10px 0 0;
+    /* margin-top: 100px; */
+  }
+}
+</style>
--- src/svelte-cositas/account.svelte
+++ src/svelte-cositas/account.svelte
@@ -1,8 +1,74 @@
 <script>
   import { routeToPage } from "$lib/routeToPage.js";
-  let locales = 
+  import Modal,{getModal} from '../lib/Modal.svelte';
+  import Index from "./index.svelte";
+
+  let locales = [
+    {
+      sucursal: 1,
+      dirección: "calle 241",
+      región: "13",
+      ciudad: "santiago",
+      resoluciones: "N° 741",
+    },
+    {
+      sucursal: 1,
+      dirección: "calle 241",
+      región: "13",
+      ciudad: "santiago",
+      resoluciones: "N° 741",
+    },
+    {
+      sucursal: 1,
+      dirección: "calle 241",
+      región: "13",
+      ciudad: "santiago",
+      resoluciones: "N° 741",
+    },
+    {
+      sucursal: 1,
+      dirección: "calle 241",
+      región: "13",
+      ciudad: "santiago",
+      resoluciones: "N° 741",
+    },
+  ];
+
+  let name = 'world';
+	
+	let selection
+	
+	// Callback function provided to the `open` function, it receives the value given to the `close` function call, or `undefined` if the Modal was closed with escape or clicking the X, etc.
+	function setSelection(res){
+		selection=res
+	}
 </script>
 
+<!-- the modal without an `id` -->
+<Modal>
+	<h1>Registrar un nuevo local {name}!</h1>
+	<!-- opening a model with an `id` and specifying a callback	 -->
+	<button on:click={()=>getModal('second').open(setSelection)}>
+		Open Nested Popup
+	</button>
+	{#if selection}
+	<p>
+		Your selection was: {selection}
+	</p>
+	{/if}
+</Modal>
+
+<Modal id="second">
+	Inner window
+	<!-- Passing a value back to the callback function	 -->
+	<button class="green" on:click={()=>getModal('second').close(1)}>
+		Select 1
+	</button>
+	<button class="green" on:click={()=>getModal('second').close(2)}>
+		Select 2
+	</button>
+	
+</Modal>
 <div class="container">
   <div class="row">
     <div class="col-4 align-center">
@@ -87,36 +153,108 @@
       />
     </div>
   </div>
-  <br>
-  <div class="row align-center">
-      
+  <br />
+  <div class="row locales-tbl">
+    <h2>Locales</h2>
     <table>
       <thead>
         <tr>
-          <th>Browser code</th>
-          <th>Browser</th>
-          <th>Browser version</th>
-          <th>Platform</th>
+          <th>Sucursal</th>
+          <th>Dirección</th>
+          <th>Región</th>
+          <th>Ciudad</th>
+          <th>Resoluciones</th>
+          <th>Ver</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td><div id="r1" /></td>
-          <td><div id="r2" /></td>
-          <td><div id="r3" /></td>
-          <td><div id="r4" /></td>
-        </tr>
+        {#each locales as local, i}
+          <tr>
+            <td>{local.sucursal}</td>
+            <td>{local.dirección}</td>
+            <td>{local.región}</td>
+            <td>{local.ciudad}</td>
+            <td>{local.resoluciones}</td>
+            <td><a href="/local">{i}</a></td>
+          </tr>
+        {/each}
       </tbody>
     </table>
+    <br />
+
+    <div class="align-center botonera">
+      <button class="red-button">
+        <!-- No logro mostrar el floppy disk -->
+        <i class="fas fa-save" /> Guardar
+      </button>
+      <button on:click={()=>getModal().open()} class="red-button"> Registrar nuevo local </button>
+    </div>
   </div>
+  <br />
+  <div class="row">
+    <div class="col-4 align-center">
+      <!-- <img class="profile-pic" src="/img/profile-holder.png" alt="aaaaa" /> -->
+      <i class="fas fa-lock big-icon" />
+      <h2>Contraseña</h2>
+    </div>
+  </div>
+  <div class="password-container">
+    <div class="row align-center ">
+      <div class="col-3">
+        <p class="label">Teléfonos:</p>
+      </div>
+      <div class="col-3">
+        <input
+          class="long-input"
+          type="password"
+          placeholder="+56 9 47823445"
+        />
+      </div>
+      <div class="col-3">
+        <p class="label">Teléfonos:</p>
+      </div>
+      <div class="col-3">
+        <input class="password" type="text" placeholder="+56 9 47823445" />
+      </div>
+      <div class="row align-center ">
+        <div class="col-1"></div>
+        <div class="col-3">
+
+          <button class="red-button">
+            <!-- No logro mostrar el floppy disk -->
+            <i class="fas fa-save" /> actualizar
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+
 </div>
 
 <style>
+  table,
+  td,
+  th {
+    border: 1px solid #ddd;
+    text-align: left;
+  }
+
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  th,
+  td {
+    padding: 5px;
+  }
   h2 {
     margin-bottom: 0px;
   }
   .container {
-    height: 120vh;
+    height: 120%;
     padding-top: 25px;
   }
   .subtitle {
@@ -139,5 +277,52 @@
   .align-center {
     display: flex;
     align-items: center;
+  }
+  .locales-tbl {
+    width: 50%;
+
+    margin: auto;
+  }
+  .red-button {
+    width: 80%;
+    height: 40px;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: 600;
+    color: #ffffff;
+    border: 3px solid #020202;
+    background-color: #be0004;
+    display: block;
+    -webkit-box-shadow: 4px 8px 19px -3px rgba(0, 0, 0, 0.27);
+    box-shadow: 4px 8px 19px -3px rgba(0, 0, 0, 0.27);
+    /* border-radius: 12px; */
+    margin-right: 20px;
+    margin-left: 20px;
+  }
+  .botonera {
+    padding: 10px;
+  }
+  .big-icon {
+    font-size: 2em;
+    margin-right: 10px;
+    margin-left: 20px;
+    color: #be0004;
+  }
+  .label {
+    font-size: 1.2em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .password-container {
+    width: 80%;
+    height: 100%;
+    border: #444444 solid 1px;
+    border-radius: 10px;
+    margin: auto;
+    margin-bottom: 20px;
+    margin-top: 10px;
+    padding: 20px;
+
   }
 </style>
